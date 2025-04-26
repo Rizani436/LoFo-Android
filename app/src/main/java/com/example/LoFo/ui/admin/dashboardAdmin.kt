67e9da6414d4d07cf2c49bfc578b058a3a3020ee
fadilfasa <fadilfasa03@gmail.com>
2025-04-26 16:07:35 +0800

    dashboard admin

---
+++ app/src/main/java/com/example/LoFo/ui/admin/dashboardAdmin.kt
@@ -0,0 +1,17 @@
+package com.example.LoFo.ui.admin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.LoFo.R
+
+class dashboardAdmin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_dashboard_admin)
+
+    }
+}
